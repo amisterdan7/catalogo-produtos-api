c2ee581ea27923a25ab635e053eff17608bd29f5
Fulano <ricardo.amisterdanservo@gmail.com>
2025-08-24 00:35:15 -0300

    Primeiro commit

---
+++ src/main/java/com/example/catalago/catalago_produtos/CatalagoProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalago.catalago_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalagoProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalagoProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catalago/catalago_produtos/CatalagoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalago.catalago_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalagoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
